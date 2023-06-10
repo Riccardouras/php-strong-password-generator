a526a0a4e6b810ccc765f2a1a57a9070f376cdf9
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-06-10 09:54:23 +0200

    creo form

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+</head>
+<body>
+    <header>
+    <header>
+        <form action="index.php" method="$_GET">
+            <label for="">Lunghezza Password</label>
+            <input type="Number" name="lunghezza" min="8">
+            <label for="">Consenti ripetizioni di uno o più caratteri</label>
+            <label for="">Sì</label>
+            <input type="radio" name="ripetizioni">
+            <label for="">No</label>
+            <input type="radio" name="ripetizioni">
+            <label for="">Lettere</label>
+            <input type="checkbox" name="lettere">
+            <label for="">Numeri</label>
+            <input type="checkbox" name="numeri">
+            <label for="">Simboli</label>
+            <input type="checkbox" name="simboli">
+        </form>
+    </header>
+    </header>
+</body>
+</html>
