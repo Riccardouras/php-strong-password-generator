9235082cf0c5988a2c555612b2f5d3c50360405e
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-06-10 16:13:32 +0200

    layout

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
     <?php
@@ -19,26 +21,47 @@
         $passwordGenerata = generaPassword($lunghezzaPassword, $consentiRipetizioni, $consentiLettere, $consentiNumeri, $consentiSimboli);
     ?>
     <header>
-        <form action="index.php" method="$_GET">
-            <label for="lunghezza">Lunghezza Password</label>
-            <input type="Number" name="lunghezza" min="8">
-            <label for="ripetizioni">Consenti ripetizioni di uno o più caratteri</label>
-            <label for="ripetizioni">Sì</label>
-            <input type="radio" name="ripetizioni">
-            <label for="ripetizioni">No</label>
-            <input type="radio" name="ripetizioni">
-            <label for="lettere">Lettere</label>
-            <input type="checkbox" name="lettere">
-            <label for="numeri">Numeri</label>
-            <input type="checkbox" name="numeri">
-            <label for="simboli">Simboli</label>
-            <input type="checkbox" name="simboli">
+        <div class="mb-5 text-center">
+            <h1 id="font">Generatore di Password</h1>
+        </div>
+        
+        <form class="flex col " action="index.php" method="$_GET">
+            <div>
+                <label for="lunghezza">Lunghezza Password:</label>
+                <input type="Number" name="lunghezza" min="8">
+            </div>
+            <div class="form-check mt-2">
+                <div>
+                    <p>Consenti ripetizioni di uno o più caratteri</p>
+                    <label for="ripetizioni">Sì</label>
+                    <input class="form-check-input" type="radio" name="ripetizioni">
+                </div>
+                <div>
+                    <label for="ripetizioni">No</label>
+                    <input  class="form-check-input" type="radio" name="ripetizioni">
+                </div>
+            </div>
+            <div class="form-check mt-2">
+                <div>
+                    <p>Scegli:</p>
+                    <label for="lettere">Lettere</label>
+                    <input class="form-check-input" type="checkbox" name="lettere">
+                </div>
+                <div>
+                    <label for="numeri">Numeri</label>
+                    <input class="form-check-input" type="checkbox" name="numeri">   
+                </div>
+                <div>
+                    <label for="simboli">Simboli</label>
+                    <input class="form-check-input" type="checkbox" name="simboli">
+                </div>
+            </div>
         </form>
     </header>
-    <main>
+    <main class="mt-4">
         <h1>
         <?php
-        echo "Password generata: " . $passwordGenerata;
+            echo "Password generata: " . $passwordGenerata;
         ?>
         </h1>
     </main>
